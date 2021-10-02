1a1152cc00044e20d769437e7dcc0becfb7cbd58
hyune <choi8608@gmail.com>
2021-10-02 17:14:18 +0900

    refactor: jav Scanner 를 nextstep Console 로 개선

--- src/main/java/input/InputService.java
+++ src/main/java/input/InputService.java
@@ -1,25 +1,23 @@
 package input;
 
 import java.util.List;
-import java.util.Scanner;
 
 import config.Property;
+import nextstep.utils.Console;
 import type.Message;
 
 public class InputService {
 
-	private final Scanner scanner;
 	private final ParseService parseService;
 
 	public InputService(final ParseService parseService) {
-		this.scanner = new Scanner(System.in);
 		this.parseService = parseService;
 	}
 
 	public List<Integer> nextIntegerList(final String printString, final int size) {
 		try {
 			System.out.print(printString);
-			return parseService.parse(scanner.nextLine(), size);
+			return parseService.parse(Console.readLine(), size);
 		} catch (final IllegalArgumentException ex) {
 			System.out.println(Message.RETRY_INPUT);
 			return nextIntegerList(printString, size);
@@ -29,7 +27,7 @@ public class InputService {
 	public int nextGameStatusTypeFlag(final String printString) {
 		try {
 			System.out.println(printString);
-			return parseService.parse(scanner.nextLine(), Property.TYPE_FLAG_LENGTH).get(0);
+			return parseService.parse(Console.readLine(), Property.TYPE_FLAG_LENGTH).get(0);
 		} catch (final IllegalArgumentException ex) {
 			System.out.println(Message.RETRY_INPUT);
 			return nextGameStatusTypeFlag(printString);
